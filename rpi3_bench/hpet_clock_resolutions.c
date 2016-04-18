083a2dd6f80faa4554c38a64fa3ebb105f89ed5d
pvphaneuf <pvphaneuf@gmail.com>
2016-04-17 18:40:42 -0700

    investigating all HPET clocks to clock resolution output

--- rpi3_bench/hpet_clock_resolutions.c
+++ rpi3_bench/hpet_clock_resolutions.c
@@ -11,10 +11,31 @@ main()
   
   struct timespec clk_res;
 
-  clock_getres(CLOCK_MONOTONIC_RAW, &clk_res);
-
+  clock_getres(CLOCK_REALTIME, &clk_res);
   ns = BILLION * clk_res.tv_sec + clk_res.tv_nsec;
-
+  printf("CLOCK_REALTIME clock resolution: %llu nanoseconds\n", (long long unsigned int) ns);
+  
+  clock_getres(CLOCK_REALTIME_COARSE, &clk_res);
+  ns = BILLION * clk_res.tv_sec + clk_res.tv_nsec;
+  printf("CLOCK_REALTIME_COARSE clock resolution: %llu nanoseconds\n", (long long unsigned int) ns);
+  
+  clock_getres(CLOCK_MONOTONIC, &clk_res);
+  ns = BILLION * clk_res.tv_sec + clk_res.tv_nsec;
+  printf("CLOCK_MONOTONIC clock resolution: %llu nanoseconds\n", (long long unsigned int) ns);
+  
+  clock_getres(CLOCK_MONOTONIC_RAW, &clk_res);
+  ns = BILLION * clk_res.tv_sec + clk_res.tv_nsec;
   printf("CLOCK_MONOTONIC_RAW clock resolution: %llu nanoseconds\n", (long long unsigned int) ns);
-
+  
+  clock_getres(CLOCK_MONOTONIC_COARSE, &clk_res);
+  ns = BILLION * clk_res.tv_sec + clk_res.tv_nsec;
+  printf("CLOCK_MONOTONIC_COARSE clock resolution: %llu nanoseconds\n", (long long unsigned int) ns);
+  
+  clock_getres(CLOCK_PROCESS_CPUTIME_ID, &clk_res);
+  ns = BILLION * clk_res.tv_sec + clk_res.tv_nsec;
+  printf("CLOCK_PROCESS_CPUTIME_ID clock resolution: %llu nanoseconds\n", (long long unsigned int) ns);
+  
+  clock_getres(CLOCK_THREAD_CPUTIME_ID, &clk_res);
+  ns = BILLION * clk_res.tv_sec + clk_res.tv_nsec;
+  printf("CLOCK_THREAD_CPUTIME_ID clock resolution: %llu nanoseconds\n", (long long unsigned int) ns);
 }
