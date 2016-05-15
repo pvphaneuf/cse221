b31f626aa11d666a765a2ea912b0c7c5d7b75d7b
pvphaneuf <pvphaneuf@gmail.com>
2016-05-15 14:33:05 -0700

    now using TCP echo service

--- rpi3_bench/network_round_trip.c
+++ rpi3_bench/network_round_trip.c
@@ -11,17 +11,26 @@
 
 #include "common.h"
 
+
 extern int errno ;
 
 
-int main(void)
+void
+print_errno(void) {
+    fprintf(stderr, "Value of errno: %d\n", errno);
+    perror("Error printed by perror");
+    fprintf(stderr, "Output by strerror: %s\n", strerror(errno));
+}
+
+
+int
+main(void)
 {
     if (init_test() != 0) {
         printf("FAILURE init_test");
+        print_errno();
         exit(EXIT_FAILURE);
     }
-
-    int errnum;
 
     int sock;
     unsigned int receive_size;
@@ -29,11 +38,11 @@ int main(void)
 
     struct sockaddr_in echoserver;
 
-    memset(&echoserver, 0, sizeof(echoserver));       /* Clear struct */
-    echoserver.sin_family = AF_INET;                  /* Internet/IP */
+    memset(&echoserver, 0, sizeof(echoserver));     /* Clear struct */
+    echoserver.sin_family = AF_INET;                /* Internet/IP */
     char local[] = "127.0.0.1";
     echoserver.sin_addr.s_addr = inet_addr(local);  /* IP address */
-    echoserver.sin_port = htons(atoi("22"));       /* server port */
+    echoserver.sin_port = htons(atoi("1000"));         /* server echo port */
 
     int send_size = 100;
 
@@ -49,18 +58,15 @@ int main(void)
 
     if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
         printf("Failed to CREATE socket");
+        print_errno();
         exit(EXIT_FAILURE);
     }
 
     received = 0;
 
-    // establish TCP socket connection
     if (connect(sock, (struct sockaddr *) &echoserver, sizeof(echoserver)) != 0) {
         printf("Failed to CONNECT to socket.\n");
-        errnum = errno;
-        fprintf(stderr, "Value of errno: %d\n", errno);
-        perror("Error printed by perror");
-        fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
+        print_errno();
         exit(EXIT_FAILURE);
     }
 
@@ -69,10 +75,7 @@ int main(void)
     printf("%i\n", bytes_sent);
 
     while (received < receive_size) {
-//        printf("here\n");
         bytes_received = recv(sock, &receive_data_buffer, receive_size, 0);
-        printf("%i\n", bytes_received);
-        printf("%s\n", receive_data_buffer);
         received += bytes_received;
     }
 
