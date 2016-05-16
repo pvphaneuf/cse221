3f03dbe33108273e4a81e83f78d93dcc78ef323b
pvphaneuf <pvphaneuf@gmail.com>
2016-05-15 18:48:55 -0700

    measuring TCP pings to localhost

--- rpi3_bench/network_round_trip.c
+++ rpi3_bench/network_round_trip.c
@@ -2,17 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>         // close()
-
 #include <errno.h>
-
-#include <netinet/in.h>     // struct sockaddr
-#include <arpa/inet.h>
+#include <netinet/in.h>     // sockaddr
+#include <arpa/inet.h>      // htons()
 #include <sys/socket.h>
+#include <time.h>   // timespec, clock_gettime()
 
 #include "common.h"
 
+#define ECHO_SERVER_PORT 7
 
-extern int errno ;
+#define TCP_TEST_COUNT 50
+
+#define MAX_PAYLOAD_SIZE 1400
+
+
+extern int errno;
 
 
 void
@@ -32,52 +37,66 @@ main(void)
         exit(EXIT_FAILURE);
     }
 
+    struct timespec start, stop;
+
     int sock;
     unsigned int receive_size;
     int received = 0;
 
-    struct sockaddr_in echoserver;
+    struct sockaddr_in echo_server_socket;
 
-    memset(&echoserver, 0, sizeof(echoserver));     /* Clear struct */
-    echoserver.sin_family = AF_INET;                /* Internet/IP */
+    memset(&echo_server_socket, 0, sizeof(echo_server_socket)); // Clear struct
+    echo_server_socket.sin_family = AF_INET;                    // Internet/IP
     char local[] = "127.0.0.1";
-    echoserver.sin_addr.s_addr = inet_addr(local);  /* IP address */
-    echoserver.sin_port = htons(atoi("1000"));         /* server echo port */
+    echo_server_socket.sin_addr.s_addr = inet_addr(local);      // IP address
+    echo_server_socket.sin_port = htons(ECHO_SERVER_PORT);      // server echo
 
-    int send_size = 100;
+    for (unsigned int send_size = 100; send_size <= MAX_PAYLOAD_SIZE; send_size += 100) {
 
-    char send_data_buffer[send_size];
-    for (int k = 0; k < send_size; k++) send_data_buffer[k] = 'b';  // stuffs output data buffer full of b's.
-    receive_size = send_size;
+        char send_data_buffer[send_size];
+        for (int k = 0; k < send_size; k++) send_data_buffer[k] = 'b';  // stuffs output data buffer full of b's.
+        receive_size = send_size;
 
-    printf("%s\n", send_data_buffer);
+        int bytes_received = 0;
+        char receive_data_buffer[receive_size];
 
-    int bytes_received = 0;
-    char receive_data_buffer[receive_size];
-    memset(&receive_data_buffer, 0, sizeof(receive_data_buffer));   // Clearing buffer.
+        clock_gettime(CLOCK_MONOTONIC_RAW, &start);
 
-    if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-        printf("Failed to CREATE socket");
-        print_errno();
-        exit(EXIT_FAILURE);
+        for (unsigned int test_idx = 0; test_idx < TCP_TEST_COUNT; test_idx += 1) {
+
+            received = 0;
+
+            if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+                printf("Failed to CREATE socket");
+                print_errno();
+                close(sock);
+                exit(EXIT_FAILURE);
+            }
+
+            if (connect(sock, (struct sockaddr *) &echo_server_socket, sizeof(echo_server_socket)) < 0) {
+                printf("Failed to CONNECT to socket.\n");
+                print_errno();
+                close(sock);
+                exit(EXIT_FAILURE);
+            }
+
+            send(sock, &send_data_buffer, send_size, 0);
+
+            while (received < receive_size) {
+                bytes_received = recv(sock, &receive_data_buffer, receive_size, 0);
+                received += bytes_received;
+            }
+
+            close(sock);
+        }
+
+        clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
+
+        const long long unsigned int total_time = 1E9 * (stop.tv_sec - start.tv_sec)
+                                                  + stop.tv_nsec - start.tv_nsec
+                                                  - GET_TIME_OVERHEAD
+                                                  - (FOR_LOOP_OVERHEAD * TCP_TEST_COUNT);
+
+        printf("send_size = %u in %f ms\n", send_size, (double)((total_time / 1E6)/ TCP_TEST_COUNT));
     }
-
-    received = 0;
-
-    if (connect(sock, (struct sockaddr *) &echoserver, sizeof(echoserver)) != 0) {
-        printf("Failed to CONNECT to socket.\n");
-        print_errno();
-        exit(EXIT_FAILURE);
-    }
-
-    int bytes_sent = send(sock, &send_data_buffer, send_size, 0);
-
-    printf("%i\n", bytes_sent);
-
-    while (received < receive_size) {
-        bytes_received = recv(sock, &receive_data_buffer, receive_size, 0);
-        received += bytes_received;
-    }
-
-    close(sock);
 }
