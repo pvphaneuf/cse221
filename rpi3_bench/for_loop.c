ba727fcbb93e9a09230d1ba4dbeaa84fc72cf78c
Patrick Phaneuf <pphaneuf@eng.ucsd.edu>
2016-06-01 03:23:57 +0000

    simplified for loop test

--- rpi3_bench/for_loop.c
+++ rpi3_bench/for_loop.c
@@ -4,118 +4,7 @@
 #include "common.h"
 
 
-int test_iteration_count_1(void)
-{
-    struct timespec start, stop;
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    for(unsigned int count = 0; count < 1; ++count){}
-    clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
-
-    double total_time = timespec_diff_to_nsecs(start, stop);
-    total_time = total_time - GET_TIME_OVERHEAD;
-
-    const double average_per_iteration_time = total_time / 1.0;
-
-    printf("FOR loop latency: %f ns with %u iterations\n", average_per_iteration_time, 1);
-
-    return 0;
-}
-
-
-int test_iteration_count_10(void)
-{
-    struct timespec start, stop;
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    for(unsigned int count = 0; count < 10; ++count){}
-    clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
-
-    double total_time = timespec_diff_to_nsecs(start, stop);
-    total_time = total_time - GET_TIME_OVERHEAD;
-
-    const double average_per_iteration_time = total_time / 10.0;
-
-    printf("FOR loop latency: %f ns with %u iterations\n", average_per_iteration_time, 10);
-
-    return 0;
-}
-
-
-int test_iteration_count_100(void)
-{
-    struct timespec start, stop;
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    for(unsigned int count = 0; count < 100; ++count){}
-    clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
-
-    double total_time = timespec_diff_to_nsecs(start, stop);
-    total_time = total_time - GET_TIME_OVERHEAD;
-
-    const double average_per_iteration_time = total_time / 100.0;
-
-    printf("FOR loop latency: %f ns with %u iterations\n", average_per_iteration_time, 100);
-
-    return 0;
-}
-
-
-int test_iteration_count_1000(void)
-{
-    struct timespec start, stop;
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    for(unsigned int count = 0; count < 1000; ++count){}
-    clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
-
-    double total_time = timespec_diff_to_nsecs(start, stop);
-    total_time = total_time - GET_TIME_OVERHEAD;
-
-    const double average_per_iteration_time = total_time / 1000.0;
-
-    printf("FOR loop latency: %f ns with %u iterations\n", average_per_iteration_time, 1000);
-
-    return 0;
-}
-
-
-int test_iteration_count_10000(void)
-{
-    struct timespec start, stop;
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    for(unsigned int count = 0; count < 10000; ++count){}
-    clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
-
-    double total_time = timespec_diff_to_nsecs(start, stop);
-    total_time = total_time - GET_TIME_OVERHEAD;
-
-    const double average_per_iteration_time = total_time / 10000.0;
-
-    printf("FOR loop latency: %f ns with %u iterations\n", average_per_iteration_time, 10000);
-
-    return 0;
-}
-
-
-int test_iteration_count_100000(void)
-{
-    struct timespec start, stop;
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    for(unsigned int count = 0; count < 100000; ++count){}
-    clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
-
-    double total_time = timespec_diff_to_nsecs(start, stop);
-    total_time = total_time - GET_TIME_OVERHEAD;
-
-    const double average_per_iteration_time = total_time / 100000.0;
-
-    printf("FOR loop latency: %f ns with %u iterations\n", average_per_iteration_time, 100000);
-
-    return 0;
-}
+#define TEST_COUNT 1000000
 
 
 int main(void) {
@@ -124,12 +13,18 @@ int main(void) {
         return -1;
     }
 
-    test_iteration_count_1();
-    test_iteration_count_10();
-    test_iteration_count_100();
-    test_iteration_count_1000();
-    test_iteration_count_10000();
-    test_iteration_count_100000();
+    struct timespec start, stop;
+
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+    for(unsigned int count = 0; count < TEST_COUNT; ++count){}
+    clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
+
+    double total_time = timespec_diff_to_nsecs(start, stop);
+    total_time = total_time - GET_TIME_OVERHEAD;
+
+    const double average_per_iteration_time = total_time / TEST_COUNT;
+
+    printf("FOR loop latency: %f ns with %u iterations\n", average_per_iteration_time, TEST_COUNT);
 
     return 0;
 }
